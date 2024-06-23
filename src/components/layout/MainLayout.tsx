80cbcea13886c5fc88f55a21a5e03620f42eaad0
hassansabbir <mahmoodsabbir3087@gmail.com>
2024-06-24 00:42:26 +0600

    setting up folder structure

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,11 @@
+const MainLayout = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">
+        Welcome to Titas Gas Adarsha High School
+      </h1>
+    </>
+  );
+};
+
+export default MainLayout;
