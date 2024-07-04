457cea1c4f9e4588c1814b47e7985466a3f835b7
Naymur Rahman <nrtusher1@gmail.com>
2024-07-04 13:05:05 +0600

    Gallery, GreetingCard, AuthritySection

--- src/pages/Home/AuthoritySection/AuthoritySection.tsx
+++ src/pages/Home/AuthoritySection/AuthoritySection.tsx
@@ -1,36 +1,20 @@
-import CardComponent from "../../../components/shared/CardComponent";
-import Container from "../../../components/ui/Container";
-import teacher1 from "../../../assets/TeacherImages/amzadSir.jpg";
 import { Fade, Slide } from "react-awesome-reveal";
+import AuthorityCard from "./AuthorityCard";
+import Container from "../../../components/ui/Container";
 
 const AuthoritySection = () => {
   return (
-    <div className="py-14 text-center">
-      <Container>
+    <Container className="text-center my-10 ">
+      {" "}
+      <section className="mb-10">
         <Slide className="text-2xl bold">Welcome to TGAHSc</Slide>
         <Fade className="font-displayTwo text-sm font-semibold">
           Some of our teacher and some words from them..
         </Fade>
-        <p></p>
-        <div className="flex gap-10 md:gap-20 my-10 flex-col md:flex-row justify-between items-center">
-          <CardComponent
-            image={teacher1}
-            shortDetails="Hi i am the head teacher of our school titas gas adarsha high school."
-            name="Amzad Takku"
-          ></CardComponent>
-          <CardComponent
-            image={teacher1}
-            shortDetails="Hi i am the head teacher of our school titas gas adarsha high school."
-            name="Amzad Takku"
-          ></CardComponent>
-          <CardComponent
-            image={teacher1}
-            shortDetails="Hi i am the head teacher of our school titas gas adarsha high school."
-            name="Amzad Takku"
-          ></CardComponent>
-        </div>
-      </Container>
-    </div>
+      </section>
+      {/* Test cards */}
+      <AuthorityCard></AuthorityCard>
+    </Container>
   );
 };
 
